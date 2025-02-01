8750624a90f59aa2ad05babc170a839793979c46
QuachTech-Community <quacktech2930@gmail.com>
2025-02-01 21:59:18 +0530

    Added layout and dashboard files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WebController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/dashboard', [WebController::class, 'dashboard']);
+
